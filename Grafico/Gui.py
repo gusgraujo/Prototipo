062d67c07a9b5470496759c63449678c1eac66fc
Gustavo <gusg.raujo@gmail.com>
2020-03-02 17:44:36 -0300

    1
    02/03/2020

--- Grafico/Gui.py
+++ Grafico/Gui.py
@@ -3,28 +3,10 @@ import numpy as np
 from tkinter import *
 from matplotlib import pyplot as plt
 from functools import partial
+from PIL import Image, ImageTk
 
 
-
-caminho = ""
-
-
-def pegar_imagem(botao):
-    wind =  Tk()
-
-    global caminho
-
-    lb = Label(wind ,  text = "Digite o caminho da imagem : ")
-    lb.pack()
-    caminho = Entry(wind, width = 20 )
-    caminho.pack()
-    bt = Button(wind, text="OK", command=wind.destroy)
-    bt.pack()
-    
-    wind.geometry("400x150")
-
-    wind.mainloop()
-
+caminho = r"C:\_SourceCode\Prototipo\lena.jpg"
 
 
 
@@ -33,17 +15,23 @@ def main_gui():
     #Cria Plat grafica
     root = Tk()
     root.title("Protótipo Íris")
-
+    root.configure(background = "blue")
+    
     buttonA = Button(root,width = 30 , text = "ADD")
     buttonA["command"] = partial(pegar_imagem,buttonA)
-    buttonA.pack(side=LEFT)
-
+    buttonA.pack(side=TOP)
     
 
-    if caminho != "" :
-        print(caminho)
-        img = cv2.imread(caminho)
-        cv2.imshow(img)
+    buttonImg = Button(root,width = 30 , text = "Show-Image")
+    buttonImg["command"] = partial(mostrar_Imagem,buttonImg)
+    buttonImg.pack(side=TOP)
+    
+ 
+    image = Image.open("C:\_SourceCode\Prototipo\lena.jpg")
+    photo = ImageTk.PhotoImage(image)
+    w = Label(image=photo)
+    w.image = photo
+    w.pack()
     
 
     #define tamanho da tela
@@ -54,5 +42,27 @@ def main_gui():
 
 
    
+def pegar_imagem(botao):
+    wind =  Tk()
 
+    lb = Label(wind ,  text = "Digite o caminho da imagem : ")
+    lb.pack()
+
+    caminho = Entry(wind, width = 20 )
+    caminho.pack()
+
+    bt = Button(wind, text="OK", command=wind.destroy)
+    bt.pack()
     
+    wind.geometry("400x150")
+    wind.mainloop()
+
+
+def mostrar_Imagem(botao):
+
+    if caminho != "" :
+        print(caminho)
+        img = cv2.imread(caminho)
+        imS = cv2.resize(img, (960, 540)) 
+        cv2.imshow("Imagem",imS)
+        cv2.waitKey()
